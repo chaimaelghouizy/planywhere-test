6c2fc2fb71b15e973f1d9676ae5425cee10725e9
chaimaelghouizy <125206986+chaimaelghouizy@users.noreply.github.com>
2024-11-10 21:03:33 +0100

    planywheretest

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SignOut from './SignOut';
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1>Planywhere Homepage </h1>
+            <p>blablablablabla...</p>
+
+
+            <SignOut/>
+        </div>
+    );
+};
+
+export default Homepage;
